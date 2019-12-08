3b297f42edee2da61f13f46e2b4d0c93864ba817
543592823 <543592823@qq.com>
2019-12-08 13:41:28 +0800

    陈星宇

--- src/main/java/com/zyc/p2p/base/util/Test2.java
+++ src/main/java/com/zyc/p2p/base/util/Test2.java
@@ -4,8 +4,8 @@ public class Test2 {
     private static DxyzTest d = new DxyzTest();
     public static void main(String[] args) {
         d.Redis();
-//        d.lj("");
-        d.cs("12345");
+//        d.lj("15886495923");
+        d.cs("510839");
     }
 
 
